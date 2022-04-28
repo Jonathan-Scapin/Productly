50f46d4506f6c922c616b331d921d988e2ca128b
Jonathan SCAPIN <jonathanscapin@mbp-de-jonathan.home>
2022-04-28 08:48:32 +0200

    add working in progress

--- scripts/main.js
+++ scripts/main.js
@@ -1,30 +1,39 @@
 function burgerMenu() {
     var x = document.getElementById("top-nav-menu");
     if (x.className === "top-nav") {
-      x.className += " responsive";
+        x.className += " responsive";
     } else {
-      x.className = "top-nav";
+        x.className = "top-nav";
     }
-  }
+}
 
-  $('.owl-carousel').owlCarousel({
-    loop:false,
-  stagePadding: 15,
-    margin:10,
-    nav:true,
-  navText : ['<span class="uk-margin-small-right uk-icon" uk-icon="icon: chevron-left"></span>','<span class="uk-margin-small-left uk-icon" uk-icon="icon: chevron-right"></span>'],
-    responsive:{
-        0:{
-            items:1
-        },
-        640:{
-            items:2
-        },
-      960:{
-            items:3
-        },
-        1200:{
-            items:4
-        }
+var asc = document.getElementById("asc");
+var dec = document.getElementById("dec");
+var col_one = document.getElementById("col-one")
+var order = 1;
+
+function changeOrder(params) {
+    col_one.style.order = 1 + order;
+    order++;
+    if (order > 3) {
+        order = 1;
     }
-})
+}
+
+const effortless_text = document.getElementById("effortless-text");
+const effortless_img = document.getElementById("effortless-img");
+const marketers_pos = document.getElementById("marketers");
+const effortless = effortless_text.offsetTop;
+const marketers = marketers_pos.offsetTop;
+
+window.onscroll = function() {load_animation()};
+
+function load_animation() {
+  if (document.documentElement.scrollTop > effortless -150) {
+    effortless_text.classList.add("toLeft");
+    effortless_img.classList.add("toRight");
+  }
+  if (document.documentElement.scrollTop > marketers -150) {
+    marketers_pos.classList.add("show");
+  }
+}
